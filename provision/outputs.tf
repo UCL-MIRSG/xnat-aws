0bc1ad189103284aa8e18351fd9889e29445bd0d
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-06-28 15:49:53 +0100

    Remove unnecessary outputs

--- provision/outputs.tf
+++ provision/outputs.tf
@@ -1,18 +1,3 @@
-output "ansible_install_dependencies" {
-  description = "Run this command from the `xnat-aws/configure` directory to install the required Ansible dependencies."
-  value       = "ansible-galaxy install -r playbooks/roles/requirements.yml --force"
-}
-
-output "ansible_install_cserv" {
-  description = "Run this command from the `xnat-aws/configure` directory to install the Container Service."
-  value       = "ansible-playbook playbooks/install_container_service.yml -i hosts.yml --vault-password-file=.vault_password"
-}
-
-output "ansible_install_xnat" {
-  description = "Run this command from the `xnat-aws/configure` directory to install and configure XNAT."
-  value       = "ansible-playbook playbooks/install_xnat.yml -i hosts.yml --vault-password-file=.vault_password"
-}
-
 output "ansible_view_vault" {
   description = "Run this command from the `xnat-aws/configure` directory to view passwords in the Ansible vault."
   value       = "ansible-vault view group_vars/all/vault --vault-password .vault_password"
