0947744d25a04987f0ec957cde99f339561f2f06
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-06-28 16:41:44 +0100

    Don't show the vault contents at the end of the install_xnat script

--- configure/install_xnat.sh
+++ configure/install_xnat.sh
@@ -8,7 +8,3 @@ ansible-playbook playbooks/install_container_service.yml -i hosts.yml --vault-pa
 
 echo "Install and configure XNAT"
 ansible-playbook playbooks/install_xnat.yml -i hosts.yml --vault-password-file=.vault_password
-
-echo "Show the passwords in the vault"
-echo "Log into the XNAT instance using the username 'admin_user' and the password 'vault_service_admin_password' stored in the vault"
-ansible-vault view group_vars/all/vault --vault-password .vault_password
