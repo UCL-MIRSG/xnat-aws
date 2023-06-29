25f41c962754de83d0e39765c15a55b0b4a262f8
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-06-29 11:41:24 +0100

    Don't create ssh key in provision

--- provision/outputs.tf
+++ provision/outputs.tf
@@ -3,10 +3,10 @@ output "ansible_install_xnat" {
   value       = "./install_xnat.sh"
 }
 
-output "ansible_view_vault" {
-  description = "Run this command from the `xnat-aws/configure` directory to view passwords in the Ansible vault."
-  value       = "ansible-vault view group_vars/all/vault --vault-password .vault_password"
-}
+
+
+
+
 
 output "xnat_web_url" {
   description = "Once XNAT has been installed and configured, the web server will be accessible at this URL."
