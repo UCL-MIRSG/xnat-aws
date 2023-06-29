afc67eb303d31098d072ed8035e25a81ac34cae3
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-06-29 11:17:31 +0100

    Use locals rather than vars for SSH key creation

--- credentials/vars.tf
+++
@@ -1,9 +0,0 @@
-variable filename {
-    type = string
-    description = "Filename in which to store the private key"
-}
-
-variable name {
-    type = string
-    description = "Name for the keypair"
-}
