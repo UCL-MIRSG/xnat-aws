360f0f5b9899abf1e1a7c6a500c9191e9249c2b7
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-06-30 17:33:09 +0100

    Add option to use rocky9 in get_ami terraform module

--- provision/modules/get_ami/get_ami.tf
+++ provision/modules/get_ami/get_ami.tf
@@ -15,6 +15,28 @@ data "aws_ami" "rhel9" {
   owners = ["309956199498"] # RedHat
 }
 
+# Get official Rocky 9 AMI for region
+data "aws_ami" "rocky9" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["Rocky-9-EC2-Base-9.2*.x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["792107900819"] # Rocky, see: https://forums.rockylinux.org/t/rocky-linux-official-aws-ami/3049/25
+}
+
 # Get official Rocky 8 AMI for region
 data "aws_ami" "rocky8" {
   most_recent = true
@@ -57,6 +79,7 @@ output "amis" {
   value = {
     "centos7" = data.aws_ami.centos7.id
     "rocky8" = data.aws_ami.rocky8.id
+    "rocky9" = data.aws_ami.rocky9.id
     "rhel9" = data.aws_ami.rhel9.id
   }
 }
