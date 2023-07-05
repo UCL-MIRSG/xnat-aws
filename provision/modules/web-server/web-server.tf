9d263bcf1a79b548a5783df4494f2aca16e2ab12
Milan Malfait <m.malfait@ucl.ac.uk>
2023-07-05 10:18:16 +0100

    Use "main" instead of "web" to refer to the main webserver

--- provision/modules/web-server/web-server.tf
+++ provision/modules/web-server/web-server.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "main" {
   availability_zone      = var.availability_zone
   subnet_id              = var.subnet_id
   private_ip             = var.private_ips["main"]
-  vpc_security_group_ids = [aws_security_group.web.id]
+  vpc_security_group_ids = [aws_security_group.main.id]
   key_name               = var.ssh_key_name
 
   tags = {
@@ -20,29 +20,45 @@ resource "aws_instance" "container" {
   instance_type = var.instance_types["container"]
 
   availability_zone      = var.availability_zone
-  subnet_id              = aws_subnet.xnat-public.id
+  subnet_id              = var.subnet_id
   private_ip             = var.private_ips["container"]
-  vpc_security_group_ids = [aws_security_group.container.id]
-  key_name               = local.ssh_key_name
+  # vpc_security_group_ids = [aws_security_group.container.id]
+  key_name               = var.ssh_key_name
 
   tags = {
     Name = var.names["container"]
   }
 }
 
-resource "aws_security_group" "web" {
-  name        = "${var.names["web"]}-sg"
+resource "aws_security_group" "main" {
+  name        = "${var.names["main"]}-sg"
   vpc_id      = var.vpc_id
   description = "Security group for the web server"
 
   tags = {
-    Name = var.names["web"]
+    Name = var.names["main"]
   }
 }
 
+resource "aws_security_group" "container" {
+  name        = "${var.names["container"]}-sg"
+  vpc_id      = var.vpc_id
+  description = "Security group for the container"
+
+  tags = {
+    Name = var.names["container"]
+  }
+}
+
+# TODO: add security group for container service
+# See if I can reuse the same security group for the container service, with something like
+# https://stackoverflow.com/questions/72871233/how-to-add-security-group-rule-to-multiple-security-groups
+
+
+
 resource "aws_security_group_rule" "allow_http_incoming" {
   type              = "ingress"
-  security_group_id = aws_security_group.web.id
+  security_group_id = aws_security_group.main.id
 
   from_port   = local.http_port
   to_port     = local.http_port
@@ -52,7 +68,7 @@ resource "aws_security_group_rule" "allow_http_incoming" {
 
 resource "aws_security_group_rule" "allow_https_incoming" {
   type              = "ingress"
-  security_group_id = aws_security_group.web.id
+  security_group_id = aws_security_group.main.id
 
   from_port   = local.https_port
   to_port     = local.https_port
@@ -62,7 +78,7 @@ resource "aws_security_group_rule" "allow_https_incoming" {
 
 resource "aws_security_group_rule" "allow_ssh_incoming" {
   type              = "ingress"
-  security_group_id = aws_security_group.web.id
+  security_group_id = aws_security_group.main.id
 
   from_port   = local.ssh_port
   to_port     = local.ssh_port
@@ -72,7 +88,7 @@ resource "aws_security_group_rule" "allow_ssh_incoming" {
 
 resource "aws_security_group_rule" "allow_all_outgoing" {
   type              = "egress"
-  security_group_id = aws_security_group.web.id
+  security_group_id = aws_security_group.main.id
 
   from_port   = local.any_port
   to_port     = local.any_port
