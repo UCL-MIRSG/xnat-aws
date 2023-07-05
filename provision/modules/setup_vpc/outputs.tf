c80a67447e237049510a23300aa6c5c40e6727c1
Milan Malfait <m.malfait@ucl.ac.uk>
2023-07-05 15:15:41 +0100

    Add public subnet ID as output for `setup_vpc`

--- provision/modules/setup_vpc/outputs.tf
+++ provision/modules/setup_vpc/outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   value = aws_vpc.xnat.id
   description = "The ID of the VPC"
 }
+
+output "public_subnet_id" {
+  value = aws_subnet.xnat-public.id
+  description = "The ID of the public subnet"
+}
