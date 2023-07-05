472aaf8cc501bf45bcfb6200f49c920254b60f7c
Milan Malfait <m.malfait@ucl.ac.uk>
2023-07-05 14:25:36 +0100

    Remove `security_groups` module
    Security groups are now defined in their respective service modules.

--- provision/modules/security_groups/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  type        = string
-  description = "The ID of the VPC"
-}
-
-variable "public_cidr" {
-  type        = list(string)
-  description = "The CIDR blcok for allowing ssh access from the public subnet"
-}
