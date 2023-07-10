89cab41a74167400b90699c7cb4863d1bc2d9b4f
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-07-10 13:39:24 +0100

    Set default database instance type to db.t3.medium

--- provision/vars.tf
+++ provision/vars.tf
@@ -75,7 +75,7 @@ variable "ec2_instance_types" {
   description = "Instance type to use for each server"
   default = {
     "xnat_web"   = "t3.medium"
-    "xnat_db"    = "t3.medium"
+    "xnat_db"    = "db.t3.medium"
     "xnat_cserv" = "t2.micro"
   }
 }
