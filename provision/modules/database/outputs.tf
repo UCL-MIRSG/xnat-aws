0bcbcc38bcecddc942b6172198d1e30f8fdb8ed3
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-07-10 13:35:39 +0100

    Add the dbname and postgres version as outputs

--- provision/modules/database/outputs.tf
+++ provision/modules/database/outputs.tf
@@ -16,6 +16,17 @@ output "xnat_db_username" {
   sensitive = true
 }
 
+output "xnat_db_name" {
+  description = "Name of the database"
+  value = aws_db_instance.db.db_name
+  sensitive = true
+}
+
+output "postgres_version" {
+  description = "Version of postgresql we're using"
+  value = aws_db_instance.db.engine_version
+}
+
 output "xnat_db_sg_id" {
   description = "RDS instance security group ID"
   value = aws_security_group.db.id
