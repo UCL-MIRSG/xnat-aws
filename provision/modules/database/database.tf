d74ee8d38140e1e69c6b8315dbc3e94f1534e1f3
Milan Malfait <m.malfait@ucl.ac.uk>
2023-07-14 10:30:24 +0100

    Force using SSL when connecting to the RDS instance (#48)

--- provision/modules/database/database.tf
+++ provision/modules/database/database.tf
@@ -5,7 +5,9 @@ resource "aws_db_instance" "db" {
   allocated_storage     = 15
   max_allocated_storage = 30
   engine                = "postgres"
-  engine_version        = "14"
+  engine_version        = local.postgres_version
+  parameter_group_name  = aws_db_parameter_group.db-parameters.name
+  apply_immediately     = true
 
   username = local.db_username
   # TODO: should we use Secrets Manager to store the password?
@@ -32,7 +34,7 @@ resource "random_password" "db_credentials" {
 resource "local_sensitive_file" "ansible-vault" {
 
   content = templatefile("templates/ansible_vault.tftpl", {
-    postgres_xnat_password      = random_password.db_credentials.result
+    postgres_xnat_password = random_password.db_credentials.result
   })
   filename        = local.ansible_vault_file
   file_permission = "0644"
@@ -75,15 +77,32 @@ resource "aws_security_group_rule" "allow_all_outgoing" {
   cidr_blocks = local.all_ips
 }
 
+# Define Database parameter group to force connections to use SSL
+resource "aws_db_parameter_group" "db-parameters" {
+  name   = "${local.db_name}-db-parameters"
+  family = "postgres${local.postgres_version}"
+
+  parameter {
+    name  = "rds.force_ssl"
+    value = "1"
+  }
+
+  # Required for modifications that force re-creation of an in-use parameter group
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 locals {
-  identifier_prefix = replace("${var.name}", "_", "-")
-  db_name = "xnat"
-  db_username   = "xnat"
-  postgres_port = 5432
-  any_port      = 0
-  tcp_protocol  = "tcp"
-  any_protocol  = "-1"
-  all_ips       = ["0.0.0.0/0"]
+  postgres_version         = "14"
+  identifier_prefix        = replace("${var.name}", "_", "-")
+  db_name                  = "xnat"
+  db_username              = "xnat"
+  postgres_port            = 5432
+  any_port                 = 0
+  tcp_protocol             = "tcp"
+  any_protocol             = "-1"
+  all_ips                  = ["0.0.0.0/0"]
   ansible_vault_file       = "../configure/group_vars/web/vault"
   encryption_password_file = "../configure/.vault_password"
 }
