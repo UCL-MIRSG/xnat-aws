0863efb07f437688df9448f846654494578d9379
Milan Malfait <m.malfait@ucl.ac.uk>
2023-08-10 09:50:43 +0200

    Resolve Terraform warnings
    Use `local_sensitive_file` with `content` attribute.
    Fixes #58

--- credentials/ssh_keygen.tf
+++ credentials/ssh_keygen.tf
@@ -3,15 +3,14 @@ resource "tls_private_key" "key" {
   algorithm = "RSA"
 }
 
-resource "local_file" "private_key" {
-  filename          = local.ssh_private_key_filename
-  sensitive_content = tls_private_key.key.private_key_openssh
-  file_permission   = "0600"
+resource "local_sensitive_file" "private_key" {
+  filename        = local.ssh_private_key_filename
+  content         = tls_private_key.key.private_key_openssh
+  file_permission = "0600"
 }
 
-resource "local_file" "public_key" {
-  filename          = local.ssh_public_key_filename
-  sensitive_content = tls_private_key.key.public_key_openssh
-  file_permission   = "0644"
+resource "local_sensitive_file" "public_key" {
+  filename        = local.ssh_public_key_filename
+  content         = tls_private_key.key.public_key_openssh
+  file_permission = "0644"
 }
-
